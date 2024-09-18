1f4dcdcdd1201a043480ae5467a6c0d0be449510
Middle Shell <denob311302@gmail.com>
2024-09-18 17:03:19 +0300

    1 task

---
+++ ProjectManagementSystem/ProjectManagementSystem/modules/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectManagementSystem.modules.authentication;
+using ProjectManagementSystem.modules.ControlData;
+using ProjectManagementSystem.modules.ControlData.Interfaces;
+using ProjectManagementSystem.modules.UserInteraction;
+
+namespace ProjectManagementSystem.modules
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем DataManager как одноэлементный объект (Singleton)
+            services.AddSingleton<DataManager>();
+            services.AddSingleton<IDataStorage, JsonDataStorage>(); // Регистрируем JsonDataStorage как реализацию IDataStorage
+            services.AddSingleton<Authenticator>();
+
+            // Регистрируем Logger как одноэлементный объект (Singleton)
+            services.AddSingleton<Logger>();
+
+            // Регистрируем CLI как объект, создаваемый для каждого запроса (Scoped)
+            services.AddScoped<CLI>();
+        }
+    }
+}
